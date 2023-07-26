5304b3a8ea910bf9c3bd522405f4d69736a7ad8d
appcode1 <mr.bq.xu@gmail.com>
2023-07-25 21:42:37 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,58 @@
-var cacheName="yi-v20230726";self.addEventListener("install",(function(e){e.waitUntil(caches.open(cacheName).then((function(e){return e.addAll(["./"])})))})),self.addEventListener("activate",(function(e){e.waitUntil(caches.keys().then((function(e){return Promise.all(e.filter((function(e){return e.startsWith("yi-")&&e!=cacheName})).map((function(e){return caches.delete(e)})))}))),e.waitUntil(caches.open(cacheName).then((function(e){e.delete("./",{ignoreSearch:!0})})))})),self.addEventListener("fetch",(function(e){e.respondWith(caches.open(cacheName).then((function(t){return t.match(e.request,{ignoreSearch:!0})})).then((function(t){return t||fetch(e.request).then((function(t){return t&&200==t.status?caches.open(cacheName).then((function(n){return n.put(e.request.url,t.clone()),t})):t}))})))})),self.addEventListener("message",(function(e){"skipWaiting"===e.data.action&&self.skipWaiting()}));
+var cacheName = 'yi-v20230726';
+self.addEventListener('install', function(event){
+	event.waitUntil(
+		caches.open(cacheName).then(function(cache){
+			return cache.addAll(['./']);
+		})
+	);
+});
+
+self.addEventListener('activate', function(event){
+	event.waitUntil(
+		var keys = caches.keys;
+		console.log('caches.keys：', keys);
+		caches.keys().then(function(keyList){
+			return Promise.all(
+				keyList.filter(function(key
+				    console.log('key:', key);
+					return key.startsWith('yi-') && key != cacheName;
+				}).map(function(key){
+					return caches.delete(key);
+				})
+			);
+		})
+	);
+	event.waitUntil(
+		caches.open(cacheName)
+		.then(function(cache){
+			//delete the cached default page (./) from the browser's local cache
+			cache.delete('./',{ignoreSearch: true});
+		})
+	);		
+});
+
+self.addEventListener("fetch", function(event) {
+	event.respondWith(
+		caches.open(cacheName)
+		.then(function(cache){return cache.match(event.request, {ignoreSearch: true});})
+		.then(function(resp){
+			if(resp) return resp;
+			return fetch(event.request).then(function(response){
+				if(response && response.status == 200) {
+					return caches.open(cacheName).then(function(cache){
+						cache.put(event.request.url, response.clone());
+						return response;
+					});
+				} else
+					return response;
+			});
+		})
+	);
+});
+
+self.addEventListener('message', function(event){
+	if(event.data.action === 'skipWaiting') {
+		self.skipWaiting();
+	}
+});
+
